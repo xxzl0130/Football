9eb0fc08d08ceca38842409112bc911bdcbfe161
zaxs0130 <zaxs0130@gmail.com>
2015-10-10 10:47:57 +0800

    Start MyDef.h for definations of var.Add MyDef.h ,AnalogyGray.h and definations of Gray Sensors in AttackMain.h

---
+++ Attack/MyDef.h
@@ -0,0 +1,16 @@
+#ifndef __MY_DEF_H__
+#define __MY_DEF_H__
+
+#ifndef uchr
+#define uchr unsigned char
+#endif
+
+#ifndef uint
+#define uint unsigned INT_MAX
+#endif
+
+#ifndef ULONG
+#define ULONG unsigned long
+#endif
+
+#endif // __MY_DEF_H__
