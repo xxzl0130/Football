2a4b2c10e219d6440925c659367ccff875dd84ed
zaxs0130 <zaxs0130@gmail.com>
2015-10-10 10:32:39 +0800

    delete MyMain.h,add definations for sensors in AttackMain.h

--- Attack/AttackMain.h
+++ Attack/AttackMain.h
@@ -3,10 +3,23 @@
 
 #include "arduino.h"
 #include "Wire.h"
+
 #include "HMC5883L.h"
 #include "IR_Eye.h"
 #include "DC_Motor.h"
 #include "US_distance.h"
 
+HMC5883L        compass;
+IR_Eye          eye(A0,10);
+
+DC_Motor_EN     xAxisMotor1(2,3,22);
+DC_Motor_EN     xAxisMotor2(4,5,23);
+DC_Motor_EN     yAxisMotor1(6,7,24);
+DC_Motor_EN     yAxisMotor2(8,9,25);
+
+US_Distance     xAxisUS1(26,27);
+US_Distance     xAxisUS2(28,29);
+US_Distance     yAxisUS1(30,31);
+US_Distance     yAxisUS2(32,33);
 
 #endif // __MY_MAIN_H__
