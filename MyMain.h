5e33e596706f538d9612b336c2123b8d5027b1c6
zaxs0130 <zaxs0130@gmail.com>
2015-10-10 10:15:55 +0800

    Start MyMain.h for this comepetition

---
+++ MyMain.h
@@ -0,0 +1,12 @@
+#ifndef __MY_MAIN_H__
+#define __MY_MAIN_H__
+
+#include "arduino.h"
+#include "Wire.h"
+#include "HMC5883L.h"
+#include "IR_Eye.h"
+#include "DC_Motor.h"
+#include "US_distance.h"
+
+
+#endif // __MY_MAIN_H__
